d19151a89d0ed3d209520a7d0b2be6a6ac26eb7f
qiweiyu <qi@weiyu.me>
2019-10-18 11:32:38 +0800

    65

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,46 @@ mod fib;
 mod queue;
 mod poker;
 
-use fraction::continued_fraction::square_root;
+extern crate num_bigint;
+
+use num_bigint::BigUint;
+use num_bigint::ToBigUint;
+use fraction::FractionBig;
 
 fn main() {
-    let mut c = 0;
-    for i in 1..=10_000 {
-        let res = square_root(i);
-        if res.1.len() % 2 == 1 {
-            c += 1;
+    let list = generate(100);
+    let len = list.len();
+    let mut res = FractionBig::new(list[len - 1].to_biguint().unwrap(), 1.to_biguint().unwrap());
+    for i in 2..=len {
+        res = list[len - i] + 1 / res;
+    }
+    res.simplify();
+    println!("{:?}", res);
+    let (s, _) = res.to_tuple_string();
+    let mut sum = 0;
+    for ch in s.as_bytes() {
+        let ch = *ch as u8 - '0' as u8;
+        sum += ch as i32;
+    }
+    println!("sum: {}", sum);
+}
+
+fn generate(limit: usize) -> Vec<u64> {
+    let mut res = vec![];
+    for i in 0..limit {
+        res.push(cal(i) as u64);
+    }
+    res
+}
+
+fn cal(idx: usize) -> usize {
+    if idx == 0 {
+        2
+    } else {
+        if idx % 3 == 2 {
+            2 * (idx / 3 + 1)
+        } else {
+            1
         }
     }
-    println!("{}", c);
 }
